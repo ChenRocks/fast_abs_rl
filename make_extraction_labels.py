cefb6ca5830e88842322e3a476231cfd7e0622b3
Yen-Chun Chen <yenchun@cs.unc.edu>
2018-05-03 16:33:55 -0700

    make extraction datasets

---
+++ make_extraction_labels.py
@@ -0,0 +1,110 @@
+"""produce the dataset with (psudo) extraction label"""
+import re
+import os
+from os.path import exists, join
+import json
+from time import time
+from datetime import timedelta
+
+from cytoolz import curry
+
+
+try:
+    DATA_DIR = os.environ['DATA']
+    DUMP_DIR = os.environ['DATASET']
+except KeyError:
+    print('please use environment variable to specify data directories')
+
+
+def _lcs_dp(a, b):
+    """ compute the len dp of lcs"""
+    dp = [[0 for _ in range(0, len(b)+1)]
+          for _ in range(0, len(a)+1)]
+    # dp[i][j]: lcs_len(a[:i], b[:j])
+    for i in range(1, len(a)+1):
+        for j in range(1, len(b)+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp
+
+def _lcs_len(a, b):
+    """ compute the length of longest common subsequence between a and b"""
+    dp = _lcs_dp(a, b)
+    return dp[-1][-1]
+
+@curry
+def compute_rouge_l(output, reference, mode='f'):
+    """ compute ROUGE-L for a single pair of summary and reference
+    output, reference are list of words
+    """
+    assert mode in list('fpr')  # F-1, precision, recall
+    lcs = _lcs_len(output, reference)
+    if lcs == 0:
+        score = 0.0
+    else:
+        precision = lcs / len(output)
+        recall = lcs / len(reference)
+        f_score = 2 * (precision * recall) / (precision + recall)
+        if mode == 'p':
+            score = precision
+        if mode == 'r':
+            score = recall
+        else:
+            score = f_score
+    return score
+
+
+def get_extract_label(art_sents, abs_sents):
+    """ greedily match summary sentences to article sentences"""
+    extracted = []
+    scores = []
+    indices = list(range(len(art_sents)))
+    for abst in abs_sents:
+        rouges = list(map(compute_rouge_l(reference=abst, mode='r'),
+                          art_sents))
+        ext = max(indices, key=lambda i: rouges[i])
+        indices.remove(ext)
+        extracted.append(ext)
+        scores.append(rouges[ext])
+        if not indices:
+            break
+    return extracted, scores
+
+def _count_data(path):
+    """ count number of data in the given path"""
+    matcher = re.compile(r'[0-9]+\.json')
+    match = lambda name: bool(matcher.match(name))
+    names = os.listdir(path)
+    n_data = len(list(filter(match, names)))
+    return n_data
+
+def label(split):
+    start = time()
+    print('start processing {} split...'.format(split))
+    data_dir = join(DATA_DIR, split)
+    dump_dir = join(DUMP_DIR, split)
+    n_data = _count_data(data_dir)
+    for i in range(n_data):
+        print('processing {}/{} ({:.2f}%%)\r'.format(i, n_data, 100*i/n_data),
+              end='')
+        with open(join(data_dir, '{}.json'.format(i))) as f:
+            data = json.loads(f.read())
+        art_sents, abs_sents = data['article'], data['abstract']
+        extracted, scores = get_extract_label(art_sents, abs_sents)
+        data['extracted'] = extracted
+        data['score'] = scores
+        with open(join(dump_dir, '{}.json'.format(i)), 'w') as f:
+            json.dump(data, f, indent=4)
+    print('finished in {}'.format(timedelta(seconds=time()-start)))
+
+
+def main():
+    for split in ['val', 'train']:  # no need of extraction label when testing
+        if not exists(join(DUMP_DIR, split)):
+            os.makedirs(join(DUMP_DIR, split))
+        label(split)
+
+if __name__ == '__main__':
+    main()
