3ee575de98ff1308065ce7fe636732bebd4f4c77
Yen-Chun Chen <yenchun@cs.unc.edu>
2018-05-04 01:33:53 -0700

    build vocabulary

--- utils.py
+++ utils.py
@@ -9,3 +9,18 @@ def count_data(path):
     names = os.listdir(path)
     n_data = len(list(filter(match, names)))
     return n_data
+
+
+PAD = 0
+UNK = 1
+START = 2
+END = 3
+def make_vocab(wc, vocab_size):
+    word2id, id2word = {}, {}
+    word2id['<pad>'] = PAD
+    word2id['<unk>'] = UNK
+    word2id['<start>'] = START
+    word2id['<end>'] = END
+    for i, (w, _) in enumerate(wc.most_common(vocab_size), 4):
+        word2id[w] = i
+    return word2id
