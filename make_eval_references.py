d2a47e88c6c0c949f574a1b16675563411063f0b
Yen-Chun Chen <yenchun@cs.unc.edu>
2018-05-17 19:13:57 -0700

    add script to make reference txt files for evaluation (ROUGE)

---
+++ make_eval_references.py
@@ -0,0 +1,40 @@
+""" make reference text files needed for ROUGE evaluation """
+import json
+import os
+from os.path import join, exists
+from time import time
+from datetime import timedelta
+
+from utils import count_data
+
+try:
+    DATA_DIR = os.environ['DATA']
+    DUMP_DIR = os.environ['DATASET']
+except KeyError:
+    print('please use environment variable to specify data directories')
+
+
+def dump(split):
+    start = time()
+    print('start processing {} split...'.format(split))
+    data_dir = join(DATA_DIR, split)
+    dump_dir = join(DUMP_DIR, 'refs', split)
+    n_data = count_data(data_dir)
+    for i in range(n_data):
+        print('processing {}/{} ({:.2f}%%)\r'.format(i, n_data, 100*i/n_data),
+              end='')
+        with open(join(data_dir, '{}.json'.format(i))) as f:
+            data = json.loads(f.read())
+        abs_sents = data['abstract']
+        with open(join(dump_dir, '{}.txt'.format(i)), 'w') as f:
+            f.write('\n'.join(abs_sents))
+    print('finished in {}'.format(timedelta(seconds=time()-start)))
+
+def main():
+    for split in ['val', 'test']:  # evaluation of train data takes too long
+        if not exists(join(DUMP_DIR, 'refs', split)):
+            os.makedirs(join(DUMP_DIR, 'refs', split))
+        dump(split)
+
+if __name__ == '__main__':
+    main()
