2064b2d8e483e2829bff76b7b95a19d68b9933af
Yen-Chun Chen <yenchun@cs.unc.edu>
2018-05-24 03:05:39 -0700

    enhence full-model decoding with reranking

--- decode_full_model.py
+++ decode_full_model.py
@@ -5,28 +5,25 @@ import os
 from os.path import join
 from datetime import timedelta
 from time import time
+from collections import Counter, defaultdict
+from itertools import product
+from functools import reduce
+import operator as op
 
-from cytoolz import identity
+from cytoolz import identity, concat, curry
 
 import torch
 from torch.utils.data import DataLoader
+from torch import multiprocessing as mp
 
 from data.batcher import tokenize
 
-from decoding import Abstractor, RLExtractor, DecodeDataset
+from decoding import Abstractor, RLExtractor, DecodeDataset, BeamAbstractor
 from decoding import make_html_safe
 
 
-# FIXME
-MAX_ART_LEN = 200  # TODO
-MAX_ART_NUM = 60
-
-MAX_ABS_NUM = 6  # need to set max sentences to extract for non-RL extractor
-
-
-def decode(save_path, model_dir, split, batch_size, beam_size, max_len, cuda):
-    if beam_size != 1:
-        raise NotImplementedError
+def decode(save_path, model_dir, split, batch_size,
+           beam_size, diverse, max_len, cuda):
     start = time()
     # setup model
     with open(join(model_dir, 'meta.json')) as f:
@@ -37,7 +34,12 @@ def decode(save_path, model_dir, split, batch_size, beam_size, max_len, cuda):
         assert beam_size == 1
         abstractor = identity
     else:
-        abstractor = Abstractor(join(model_dir, 'abstractor'), max_len, cuda)
+        if beam_size == 1:
+            abstractor = Abstractor(join(model_dir, 'abstractor'),
+                                    max_len, cuda)
+        else:
+            abstractor = BeamAbstractor(join(model_dir, 'abstractor'),
+                                        max_len, cuda)
     extractor = RLExtractor(model_dir, cuda=cuda)
 
     # setup loader
@@ -61,6 +63,7 @@ def decode(save_path, model_dir, split, batch_size, beam_size, max_len, cuda):
     dec_log['rl'] = True
     dec_log['split'] = split
     dec_log['beam'] = beam_size
+    dec_log['diverse'] = diverse
     with open(join(save_path, 'log.json'), 'w') as f:
         json.dump(dec_log, f, indent=4)
 
@@ -73,25 +76,66 @@ def decode(save_path, model_dir, split, batch_size, beam_size, max_len, cuda):
             ext_inds = []
             for raw_art_sents in tokenized_article_batch:
                 ext = extractor(raw_art_sents)[:-1]  # exclude EOE
+                if not ext:
+                    # use top-5 if nothing is extracted
+                    # in some rare cases rnn-ext does not extract at all
+                    ext = list(range(5))[:len(raw_art_sents)]
+                else:
+                    ext = [i.item() for i in ext]
                 ext_inds += [(len(ext_arts), len(ext))]
-                ext_arts += list(map(lambda i: raw_art_sents[i.item()], ext))
-            dec_outs = abstractor(ext_arts)
-            assert i == batch_size*i_debug
+                ext_arts += [raw_art_sents[i] for i in ext]
             if beam_size > 1:
-                # TODO
-                pass  # reranking model
+                all_beams = abstractor(ext_arts, beam_size, diverse)
+                dec_outs = rerank_mp(all_beams, ext_inds)
             else:
-                for j, n in ext_inds:
-                    decoded_sents = [' '.join(dec) for dec in dec_outs[j:j+n]]
-                    with open(join(save_path, 'output/{}.dec'.format(i)),
-                              'w') as f:
-                        f.write(make_html_safe('\n'.join(decoded_sents)))
-                    i += 1
-                    print('{}/{} ({:.2f}%) decoded in {} seconds\r'.format(
-                        i, n_data, i/n_data*100,
-                        timedelta(seconds=int(time()-start))
-                    ), end='')
+                dec_outs = abstractor(ext_arts)
+            assert i == batch_size*i_debug
+            for j, n in ext_inds:
+                decoded_sents = [' '.join(dec) for dec in dec_outs[j:j+n]]
+                with open(join(save_path, 'output/{}.dec'.format(i)),
+                          'w') as f:
+                    f.write(make_html_safe('\n'.join(decoded_sents)))
+                i += 1
+                print('{}/{} ({:.2f}%) decoded in {} seconds\r'.format(
+                    i, n_data, i/n_data*100,
+                    timedelta(seconds=int(time()-start))
+                ), end='')
     print()
+
+_PRUNE = defaultdict(
+    lambda: 2,
+    {1:5, 2:5, 3:5, 4:5, 5:5, 6:4, 7:3, 8:3}
+)
+
+def rerank(all_beams, ext_inds):
+    beam_lists = (all_beams[i: i+n] for i, n in ext_inds if n > 0)
+    return list(concat(map(rerank_one, beam_lists)))
+
+def rerank_mp(all_beams, ext_inds):
+    beam_lists = [all_beams[i: i+n] for i, n in ext_inds if n > 0]
+    with mp.Pool(8) as pool:
+        reranked = pool.map(rerank_one, beam_lists)
+    return list(concat(reranked))
+
+def rerank_one(beams):
+    @curry
+    def process_beam(beam, n):
+        for b in beam[:n]:
+            b.gram_cnt = Counter(_make_n_gram(b.sequence))
+        return beam[:n]
+    beams = map(process_beam(n=_PRUNE[len(beams)]), beams)
+    best_hyps = max(product(*beams), key=_compute_score)
+    dec_outs = [h.sequence for h in best_hyps]
+    return dec_outs
+
+def _make_n_gram(sequence, n=2):
+    return (tuple(sequence[i:i+n]) for i in range(len(sequence)-(n-1)))
+
+def _compute_score(hyps):
+    all_cnt = reduce(op.iadd, (h.gram_cnt for h in hyps), Counter())
+    repeat = sum(c-1 for g, c in all_cnt.items() if c > 1)
+    lp = sum(h.logprob for h in hyps) / sum(len(h.sequence) for h in hyps)
+    return (-repeat, lp)
 
 
 if __name__ == '__main__':
@@ -108,6 +152,8 @@ if __name__ == '__main__':
                         help='batch size of faster decoding')
     parser.add_argument('--beam', type=int, action='store', default=1,
                         help='beam size for beam-search (reranking included)')
+    parser.add_argument('--div', type=float, action='store', default=1.0,
+                        help='diverse ratio for the diverse beam-search')
     parser.add_argument('--max_dec_word', type=int, action='store', default=30,
                         help='maximun words to be decoded for the abstractor')
 
@@ -118,4 +164,5 @@ if __name__ == '__main__':
 
     data_split = 'test' if args.test else 'val'
     decode(args.path, args.model_dir,
-           data_split, args.batch, args.beam, args.max_dec_word, args.cuda)
+           data_split, args.batch, args.beam, args.div,
+           args.max_dec_word, args.cuda)
